6b367a465ac3e698b0e0ed1d5db9468e81c36070
Your Name <you@example.com>
2020-02-09 20:12:52 +0100

    minor changes

--- delay.sh
+++ delay.sh
@@ -2,12 +2,12 @@
 
 interface=enp0s3
 ip=(104.24.96.159 195.128.101.122)
-delay=1ms
+delay=100ms
 
-sudo tc qdisc replace dev $interface root handle 1: prio
+sudo tc qdisc add dev $interface root handle 1: prio
 for i in ${ip[@]}
 do
-    sudo tc filter replace dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $i flowid 2:1
+    sudo tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $i flowid 2:1
     echo set filter for $i
 done
-sudo tc qdisc replace dev $interface parent 1:1 handle 2: netem delay $delay
+sudo tc qdisc add dev $interface parent 1:1 handle 2: netem delay $delay
