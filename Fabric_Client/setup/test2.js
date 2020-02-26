8269800c581e528ae6eaaf5246709763d2d6ab35
Your Name <you@example.com>
2020-02-26 22:19:14 +0100

    added ccquery

--- Fabric_Client/setup/test2.js
+++ Fabric_Client/setup/test2.js
@@ -22,7 +22,7 @@ async function test(){
     //initializing the contracts
     await bench.init().catch(err => {console.log(err)})
 
-    result = await bench.ccQueryPublice("", "")
+    result = await bench.complexQueryPublic("165")
     console.log(result)
     process.exit()
 
